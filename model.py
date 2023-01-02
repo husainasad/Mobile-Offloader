d4580dcbaedb7b39e963bb6e294459416fb7682a
unknown <asad1999@gmail.com>
2023-01-01 23:04:26 -0700

    implemented k-fold cross-validation

--- model.py
+++ model.py
@@ -110,16 +110,37 @@ def checkpoint(saveweight):
     save_best_only=True)
     return model_checkpoint_callback
 
-def modelEvaluation(dataX, dataY, saveweight):
+# def modelEvaluation(dataX, dataY, saveweight):
+#     scores, histories = list(), list()
+#     trainX, testX, trainY, testY = train_test_split(dataX, dataY, test_size=0.2, random_state=42)
+#     model = constructModel()
+#     callback_checkpoint = checkpoint(saveweight)
+#     history = model.fit(trainX, trainY, epochs=10, batch_size=32, validation_data=(testX, testY), verbose=0, callbacks=[callback_checkpoint])
+#     _, acc = model.evaluate(testX, testY, verbose=0)
+#     print('> %.3f' % (acc * 100.0))
+#     scores.append(acc)
+#     histories.append(history)
+#     return scores, histories
+
+def modelEvaluation(dataX, dataY, saveweight, n_folds =5):
     scores, histories = list(), list()
-    trainX, testX, trainY, testY = train_test_split(dataX, dataY, test_size=0.2, random_state=42)
-    model = constructModel()
-    callback_checkpoint = checkpoint(saveweight)
-    history = model.fit(trainX, trainY, epochs=10, batch_size=32, validation_data=(testX, testY), verbose=0, callbacks=[callback_checkpoint])
-    _, acc = model.evaluate(testX, testY, verbose=0)
-    print('> %.3f' % (acc * 100.0))
-    scores.append(acc)
-    histories.append(history)
+    # prepare cross validation
+    kfold = KFold(n_folds, shuffle=True, random_state=1)
+    # enumerate splits
+    for train_ix, test_ix in kfold.split(dataX):
+      # define model
+      model = constructModel()
+      # select rows for train and test
+      trainX, trainY, testX, testY = dataX[train_ix], dataY[train_ix], dataX[test_ix], dataY[test_ix]
+      # fit model
+      callback_checkpoint = checkpoint(saveweight)
+      history = model.fit(trainX, trainY, epochs=10, batch_size=32, validation_data=(testX, testY), verbose=0, callbacks=[callback_checkpoint])
+      # evaluate model
+      _, acc = model.evaluate(testX, testY, verbose=0)
+      print('> %.3f' % (acc * 100.0))
+      # stores scores
+      scores.append(acc)
+      histories.append(history)
     return scores, histories
 
 
